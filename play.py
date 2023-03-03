6e308086badc1e421f9e76bcfd8efd9e9f565229
Ben Wiedermann <benw@cs.hmc.edu>
2023-03-02 19:47:25 -0800

    Reworking for Google colab

--- play.py
+++ play.py
@@ -1,6 +1,10 @@
 from util import *
+import pandas as pd
 
-# describes relationships that various parts of the play can have
+
+################################################################################
+# Describes relationships that various parts of the play can have
+################################################################################
 
 
 @containsNumberedField('act')
@@ -28,8 +32,11 @@ class HasCharacters(object):
         setattr(self, character.name, character)
         super().addCharacter(character)
 
+################################################################################
+# Describes the parts of the play
+################################################################################
 
-# describes the parts of the play
+
 class Play(Named, HasCharacters, HasActs):
     @property
     def scenes(self):
@@ -63,6 +70,19 @@ class Character(Named, HasSpeeches):
         return sorted(set([speech.scene for speech in self.speeches]),
                       key=lambda scene: (scene.act.number, scene.number))
 
+    @property
+    def numScenes(self): return len(self.scenes)
+
+    @property
+    def numSpeeches(self): return len(self.speeches)
+
+    @property
+    def numLines(self): return sum(len(speech) for speech in self.speeches)
+
+    @property
+    def numWords(self): return sum(len(speech.words)
+                                   for speech in self.speeches)
+
     def __str__(self):
         return self.name
 
@@ -92,3 +112,63 @@ class Speech(object):
 
     def __repr__(self):
         return str((self.character, len(self), self.text, self.startline))
+
+################################################################################
+# Some helper functions for working with plays
+################################################################################
+
+
+def isInScene(character, scene):
+    return character in scene.characters
+
+
+def speechesInScene(character, scene):
+    return [speech for speech in scene.speeches if speech.character is character]
+
+
+def speechesPerScene(character, scene):
+    characterSpeeches = speechesInScene(character, scene)
+    return len(characterSpeeches)
+
+
+def linesPerScene(character, scene):
+    characterSpeeches = speechesInScene(character, scene)
+    return sum(map(len, characterSpeeches))
+
+
+def wordsPerScene(character, scene):
+    characterSpeeches = speechesInScene(character, scene)
+    return sum(map(lambda s: len(s.words), characterSpeeches))
+
+
+def characterTable(play):
+    characters = sorted(list(play.characters), key=lambda c: c.name)
+    character_names = [character.name for character in characters]
+
+    def get_data(extractor):
+        return [extractor(character) for character in characters]
+
+    character_data = pd.DataFrame(index=character_names)
+    character_data['# of scenes'] = get_data(lambda c: c.numScenes)
+    character_data['# of speeches'] = get_data(lambda c: c.numSpeeches)
+    character_data['# of lines'] = get_data(lambda c: c.numLines)
+    character_data['# of words'] = get_data(lambda c: c.numWords)
+    return character_data
+
+
+def playTable(play):
+    scenes = play.scenes
+    characters = sorted(list(play.characters), key=lambda c: c.name)
+    character_names = [character.name for character in characters]
+
+    def get_data(extractor):
+        return [[extractor(character, scene)
+                 for scene in scenes]
+                for character in characters]
+
+    play_data = pd.DataFrame(index=character_names)
+    play_data['appearances'] = get_data(isInScene)
+    play_data['speeches / scene'] = get_data(speechesPerScene)
+    play_data['lines / scene'] = get_data(linesPerScene)
+    play_data['words / scene'] = get_data(wordsPerScene)
+    return play_data
--- visualizations.py
+++ visualizations.py
@@ -1,10 +1,12 @@
 from plotly.graph_objs import Bar, Histogram, Layout, Heatmap
 from plotly.tools import FigureFactory
 from operator import attrgetter
+from play import isInScene, speechesPerScene, linesPerScene, wordsPerScene
 
-#========================================
+# ========================================
 # Bar Plots
-#========================================
+# ========================================
+
 
 def barLayout(title):
     return Layout(
@@ -16,8 +18,8 @@ def barLayout(title):
                 color='lightgrey'
             ),
             showticklabels=True,
-            #tick0=0,
-            #dtick=1,
+            # tick0=0,
+            # dtick=1,
             tickfont=dict(
                 family='Old Standard TT, serif',
                 size=8,
@@ -42,129 +44,129 @@ def barLayout(title):
         )
     )
 
+
 def horizontalPlot(xValues, yValues, title=None):
     data = [
         Bar(
-            x = xValues,
-            y = yValues,
-            orientation = 'h',
+            x=xValues,
+            y=yValues,
+            orientation='h',
         )
     ]
-    
-    return {'data':data, 'layout':barLayout(title)} 
 
-def characterLinePlot(play):
-    data = sorted([(character.lines(), character.name) for character in play.characters])
-    lines, names = zip(*data)
-    return horizontalPlot(lines, names, "lines")
-
-def characterSpeechPlot(play):
-    data = sorted([(len(character.speeches), character.name) for character in play.characters])
-    speeches, names = zip(*data)
-    return horizontalPlot(speeches, names, "speeches")
-
-def characterScenePlot(play):
-    data = sorted([(len(character.scenes), character.name) for character in play.characters])
-    scenes, names = zip(*data)
-    return horizontalPlot(scenes, names, "scenes")
+    return {'data': data, 'layout': barLayout(title)}
 
 
-#========================================
+def characterPlot(play, extractor, title, characters=None):
+    if not characters:
+        characters = play.characters
+
+    data = sorted([(extractor(character), character.name)
+                  for character in characters])
+    values, names = zip(*data)
+    return horizontalPlot(values, names, title)
+
+
+def characterLinePlot(play, characters=None):
+    return characterPlot(play, lambda c: c.numLines, 'lines', characters)
+
+
+def characterSpeechPlot(play, characters=None):
+    return characterPlot(play, lambda c: c.numSpeeches, 'speeches', characters)
+
+
+def characterScenePlot(play, characters=None):
+    return characterPlot(play, lambda c: c.numScenes, 'scenes', characters)
+
+
+# ========================================
 # Character heat maps
-#========================================
-def isInScene(character, scene):
-    return int(character in scene.characters)
-
-def speechesPerScene(character, scene):
-    characterSpeeches = [speech for speech in scene.speeches if speech.character is character]
-    return len(characterSpeeches)
-
-def linesPerScene(character, scene):
-    characterSpeeches = [speech for speech in scene.speeches if speech.character is character]
-    return sum(map(len, characterSpeeches))
-
-def wordsPerScene(character, scene):
-    characterSpeeches = [speech for speech in scene.speeches if speech.character is character]
-    return sum(map(lambda s: len(s.words), characterSpeeches))
+# ========================================
 
 def heatMap(x, y, z, title=None):
     #colorscale = [[0, 'rgb(250,250,250)'], [1, 'rgb(255, 153, 0)']]
     colorscale = [[0, 'rgb(250,250,250)'], [1, 'rgb(0, 150, 255)']]
-    
+
     data = [
         Heatmap(
             x=x,
             y=y,
             z=z,
             colorscale=colorscale,
-            colorbar = dict(outlinewidth=0, outlinecolor='#cfcfcf'),            
+            colorbar=dict(outlinewidth=0, outlinecolor='#cfcfcf'),
         )
     ]
 
-    defaultAxis = dict( type='category',
-                        showticklabels=True,
-                        tickfont=dict(
+    defaultAxis = dict(type='category',
+                       showticklabels=True,
+                       tickfont=dict(
                             family='Old Standard TT, serif',
                             size=8,
                             color='black'
-                        ),
-                    )
-    
+                       ),
+                       )
+
     topAxis = defaultAxis.copy()
-    topAxis.update( dict(side='top') )
-    
+    topAxis.update(dict(side='top'))
+
     bottomAxis = defaultAxis.copy()
-    bottomAxis.update( dict(side='bottom', overlaying='x') )
+    bottomAxis.update(dict(side='bottom', overlaying='x'))
 
     layout = Layout(
-        xaxis = topAxis,
-        xaxis2 = bottomAxis,
-        yaxis = defaultAxis,
-        title = title,
+        xaxis=topAxis,
+        xaxis2=bottomAxis,
+        yaxis=defaultAxis,
+        title=title,
         # height = 200,
     )
-    
+
     return {'data': data, 'layout': layout}
 
 
 def characterMap(play, metric=isInScene, characters=None):
-    
+
     if not characters:
         characters = play.characters
 
     sortedCharacters = sorted(characters, key=lambda c: len(c.scenes))
 
     # for the horizontal axis
-    sceneNames = sorted(['%d.%d' % (scene.act.number, scene.number) for scene in play.scenes])
-    
+    sceneNames = sorted(['%d.%d' % (scene.act.number, scene.number)
+                        for scene in play.scenes])
+
     # for the vertical axis
     characterNames = [character.name for character in sortedCharacters]
-    
+
     # for the value axis
-    intensities = [[metric(character, scene) for scene in play.scenes] 
+    intensities = [[metric(character, scene) for scene in play.scenes]
                    for character in sortedCharacters]
 
     return heatMap(sceneNames, characterNames, intensities)
 
-def metrics(play, characters=None):
-    
-    numScenes = len(play.scenes)
+# ========================================
+# Character comparison
+# ========================================
 
-    if not characters:
-        characters = play.characters
-    #characters = sorted(play.characters, key=lambda c: len(c.lines()))
 
-    results = ['Role,Speeches,Lines,Words,Scenes']
-    for character in characters:
-        characterScenes = len(character.scenes)
-        characterName = character.name
-        characterSpeeches = len(character.speeches)
-        characterLines = character.lines()
-        characterWords = character.words()
-        results.append('%s,%d,%d,%d,%d' %
-                       (characterName, characterSpeeches, characterLines, characterWords, characterScenes))
+def characterCompare(play, character1, character2, metric=speechesPerScene):
+    c1 = [c for c in play.characters if c.name == character1][0]
+    c2 = [c for c in play.characters if c.name == character2][0]
 
-    return '\n'.join(results)
+    # for the horizontal axis
+    sceneNames = sorted(
+        [f'{scene.act.number}.{scene.number}' for scene in play.scenes])
+
+    # for the value axis
+    c1Values = [metric(c1, scene) for scene in play.scenes]
+    c2Values = [-1*metric(c2, scene) for scene in play.scenes]
+
+    data = [Bar(x=sceneNames, y=c1Values, orientation='v', name=character1.title()),
+            Bar(x=sceneNames, y=c2Values, orientation='v', name=character2.title())]
+
+    layout = Layout(barmode='relative', xaxis=dict(type='category'))
+
+    return {'data': data, 'layout': layout}
+
 
 def characterPairingsMap(play, characters=None):
     if not characters:
@@ -172,35 +174,14 @@ def characterPairingsMap(play, characters=None):
     sortedCharacters = sorted(characters, key=lambda c: len(c.scenes))
     pairings = {}
     for character in sortedCharacters:
-        pairings[character] = {c:0 for c in sortedCharacters}
+        pairings[character] = {c: 0 for c in sortedCharacters}
         for scene in character.scenes:
             for other in [c for c in scene.characters if c in sortedCharacters]:
                 if other is not character:
                     pairings[character][other] += 1
 
     names = [c.name for c in sortedCharacters]
-    intensities = [[pairings[c][o] for o in reversed(sortedCharacters)] for c in sortedCharacters]
+    intensities = [[pairings[c][o]
+                    for o in reversed(sortedCharacters)] for c in sortedCharacters]
     # return heatMap(list(reversed(names)), [names[-2], names[-3]], [intensities[-2], intensities[-3]])
     return heatMap(list(reversed(names)), names, intensities)
-    
-#========================================
-# Character comparison
-#========================================
-
-def characterCompare(play, character1, character2, metric=speechesPerScene):
-    c1 = [c for c in play.characters if c.name == character1][0]
-    c2 = [c for c in play.characters if c.name == character2][0]
-
-    # for the horizontal axis
-    sceneNames = sorted([f'{scene.act.number}.{scene.number}' for scene in play.scenes])
-
-    # for the value axis
-    c1Values = [metric(c1, scene) for scene in play.scenes]
-    c2Values = [-1*metric(c2, scene) for scene in play.scenes]
-
-    data = [Bar(x=sceneNames, y=c1Values, orientation='v', name=character1.title()), 
-            Bar(x=sceneNames, y=c2Values, orientation='v', name=character2.title())]
-
-    layout = Layout(barmode='relative', xaxis=dict(type='category'))
-
-    return {'data':data, 'layout':layout}
