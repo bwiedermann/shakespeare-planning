bb5580ab94f948c64d41da42e3bcc618f91c68d4
Ben Wiedermann <benw@cs.hmc.edu>
2023-03-02 18:29:12 -0800

    Add __str__ to some items

--- play.py
+++ play.py
@@ -1,13 +1,17 @@
 from util import *
 
 # describes relationships that various parts of the play can have
+
+
 @containsNumberedField('act')
 class HasActs(object):
     pass
 
+
 @containsNumberedField('scene')
 class HasScenes(object):
     pass
+
 
 @containsListField('speech', 'speeches')
 class HasSpeeches(object):
@@ -16,6 +20,7 @@ class HasSpeeches(object):
 
     def words(self):
         return sum(len(speech.words) for speech in self.speeches)
+
 
 @containsSetField('character')
 class HasCharacters(object):
@@ -33,19 +38,34 @@ class Play(Named, HasCharacters, HasActs):
             result.extend(act.scenes)
         return result
 
+    def __str__(self):
+        return self.name
+
+
 class Act(Numbered, HasCharacters, HasScenes):
     pass
 
+    def __str__(self):
+        return f'{self.number}'
+
+
 class Scene(Numbered, HasCharacters, HasSpeeches):
     pass
+
+    def __str__(self):
+        return f'{self.act}.{self.number}'
+
 
 @registered
 class Character(Named, HasSpeeches):
     @property
     def scenes(self):
         return sorted(set([speech.scene for speech in self.speeches]),
-                      key = lambda scene: (scene.act.number, scene.number))
-    
+                      key=lambda scene: (scene.act.number, scene.number))
+
+    def __str__(self):
+        return self.name
+
 
 class Speech(object):
     def __init__(self, character, act, scene, startline=0, text=''):
@@ -57,17 +77,18 @@ class Speech(object):
         self.lines = text.split('\n')
         self.words = text.split()
         self.endline = startline + len(self)
-        
+
     def __len__(self):
         return len(self.lines)
-    
+
     def __str__(self):
         output = [self.character]
         for lineNumber, line in enumerate(self.lines):
             actualLineNumber = lineNumber+self.startline
-            numberString = str(actualLineNumber) if (actualLineNumber % 10 == 0) else ''
+            numberString = str(actualLineNumber) if (
+                actualLineNumber % 10 == 0) else ''
             output.append('{:77}{:>3}'.format(line, numberString))
         return '\n'.join(output)
-    
+
     def __repr__(self):
-        return str( (self.character, len(self), self.text, self.startline) )
+        return str((self.character, len(self), self.text, self.startline))
